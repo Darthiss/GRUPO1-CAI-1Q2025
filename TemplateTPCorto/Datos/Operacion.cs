25b07e4a61ec383ba6e5ef499491178bd9d617af
rociocarballeira <rociomcarballeira@gmail.com>
2025-05-18 20:08:24 -0300

    creacion clases operaciones

---
+++ TemplateTPCorto/Datos/Operacion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Operacion
+    {
+        public string IdOperacion { get; set; }
+        public string TipoOperacion { get; set; } // "ModificarPersona", "CambioCredencial"
+        public string Estado { get; set; } // "Pendiente", "Autorizada", "Rechazada"
+        public string LegajoSolicitante { get; set; }
+        public DateTime FechaSolicitud { get; set; }
+        public string LegajoAutorizador { get; set; } // puede ser null
+        public DateTime? FechaAutorizacion { get; set; } // puede ser null
+        public override string ToString()
+        {
+            string fechaAutorizacionStr = FechaAutorizacion.HasValue ? FechaAutorizacion.Value.ToString("dd/MM/yyyy") : "";
+            string legajoAutorizadorStr = LegajoAutorizador ?? "";
+
+            return $"{IdOperacion};{TipoOperacion};{Estado};{LegajoSolicitante};{FechaSolicitud:dd/MM/yyyy};{legajoAutorizadorStr};{fechaAutorizacionStr}";
+        }
+    }
+
+}
